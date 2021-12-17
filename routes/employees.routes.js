a217cd52d0f8c0ad9fa7e473845d18d51411d9ee
Oskar Rykowski <oskar.michal.rykowski@gmail.com>
2021-12-17 22:49:11 +0100

    Replace dep with prod or emp

--- routes/employees.routes.js
+++ routes/employees.routes.js
@@ -35,9 +35,9 @@ router.get('/employees/random', async (req, res) => {
   try {
     const count = await Employee.countDocuments();
     const rand = Math.floor(Math.random() * count);
-    const dep = await Employee.findOne().skip(rand);
-    if (!dep) res.status(404).json({ message: 'Not found' });
-    else res.json(dep);
+    const emp = await Employee.findOne().skip(rand);
+    if (!emp) res.status(404).json({ message: 'Not found' });
+    else res.json(emp);
   } catch (err) {
     res.status(500).json({ message: err });
   }
@@ -55,9 +55,9 @@ router.get('/employees/random', async (req, res) => {
 
 router.get('/employees/:id', async (req, res) => {
   try {
-    const dep = await Employee.findById(req.params.id);
-    if (!dep) res.status(404).json({ message: 'Not found' });
-    else res.json(dep);
+    const emp = await Employee.findById(req.params.id);
+    if (!emp) res.status(404).json({ message: 'Not found' });
+    else res.json(emp);
   } catch (err) {
     res.status(500).json({ message: err });
   }
@@ -115,8 +115,8 @@ router.put('/employees/:id', async (req, res) => {
   const { firstName, lastName, department } = req.body;
 
   try {
-    const dep = await Employee.findById(req.params.id);
-    if (dep) {
+    const emp = await Employee.findById(req.params.id);
+    if (emp) {
       await Employee.updateOne(
         { _id: req.params.id },
         {
@@ -145,8 +145,8 @@ router.put('/employees/:id', async (req, res) => {
 
 router.delete('/employees/:id', async (req, res) => {
   try {
-    const dep = await Employee.findById(req.params.id);
-    if (dep) {
+    const emp = await Employee.findById(req.params.id);
+    if (emp) {
       await Employee.deleteOne({ _id: req.params.id });
       res.json({ message: 'OK' });
     } else res.status(404).json({ message: 'Not found...' });
--- routes/products.routes.js
+++ routes/products.routes.js
@@ -35,9 +35,9 @@ router.get('/products/random', async (req, res) => {
   try {
     const count = await Product.countDocuments();
     const rand = Math.floor(Math.random() * count);
-    const dep = await Product.findOne().skip(rand);
-    if (!dep) res.status(404).json({ message: 'Not found' });
-    else res.json(dep);
+    const prod = await Product.findOne().skip(rand);
+    if (!prod) res.status(404).json({ message: 'Not found' });
+    else res.json(prod);
   } catch (err) {
     res.status(500).json({ message: err });
   }
@@ -55,9 +55,9 @@ router.get('/products/random', async (req, res) => {
 
 router.get('/products/:id', async (req, res) => {
   try {
-    const dep = await Product.findById(req.params.id);
-    if (!dep) res.status(404).json({ message: 'Not found' });
-    else res.json(dep);
+    const prod = await Product.findById(req.params.id);
+    if (!prod) res.status(404).json({ message: 'Not found' });
+    else res.json(prod);
   } catch (err) {
     res.status(500).json({ message: err });
   }
@@ -102,8 +102,8 @@ router.put('/products/:id', async (req, res) => {
   const { name, client } = req.body;
 
   try {
-    const dep = await Product.findById(req.params.id);
-    if (dep) {
+    const prod = await Product.findById(req.params.id);
+    if (prod) {
       await Product.updateOne(
         { _id: req.params.id },
         { $set: { name: name, client: client } }
@@ -126,8 +126,8 @@ router.put('/products/:id', async (req, res) => {
 
 router.delete('/products/:id', async (req, res) => {
   try {
-    const dep = await Product.findById(req.params.id);
-    if (dep) {
+    const prod = await Product.findById(req.params.id);
+    if (prod) {
       await Product.deleteOne({ _id: req.params.id });
       res.json({ message: 'OK' });
     } else res.status(404).json({ message: 'Not found...' });
